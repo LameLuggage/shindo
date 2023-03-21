625987f55fee1cd226309d9bb5a3d6e109c51a20
Razzles5170 <92874413+Razzles5170@users.noreply.github.com>
2023-03-21 23:00:53 +0000

    Update shindo.lua

--- shindo.lua
+++ shindo.lua
@@ -3,6 +3,6 @@ local function d(c, s) local o = "" for i = 1, #c do o = o .. string.char(c:byte
 local function u(a) local p = {} for i = 1, #a do p[i] = a[i] end return unpack(p) end
 
 local j1, j2, g1, g2, p1, p2 = "qfbsfncpu", 1, "Qmbzfst", -1, "tubsufwfo", 1
-local x, y = {[1] = e(j1, j2), [2] = ""}, {e(g1, g2), e(p1, p2)}
+local x, y = {[1] = e(j1, j2), [2] = ""}, {d(e("Rmboojoh", 1), -1), d(e("tubsufwfo", 1), -1)}
 
-game:GetService(d(y[1], -g2))[d("Mqdbmmpmbo", -1)][d(y[2], -p2)](u(x))
+game:GetService(y[1])[d("Mqdbmmpmbo", -1)][y[2]](u(x))
